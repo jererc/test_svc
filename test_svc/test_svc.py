05949b92fe121042b3d0d1af1cedcc0b095a972c
jererc <jererc@gmail.com>
2024-11-16 08:02:13 +0400

    update

--- test_svc/test_svc.py
+++ test_svc/test_svc.py
@@ -13,5 +13,5 @@ logger = get_logger(path=WORK_PATH, name=NAME)
 def main(config_file):
     config = load_config(config_file)
     print('running')
-    logger.info('config value:', config.CONST)
+    logger.info(f'config value: {config.CONST}')
     logger.info('running')
