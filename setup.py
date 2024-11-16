d478dbc4ef004f71594d945bdd606193c29222f1
jererc <jererc@gmail.com>
2024-11-16 07:44:18 +0400

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/test_svc',
-    packages=find_packages(exclude=['tests', 'scripts']),
+    packages=find_packages(exclude=['tests']),
     python_requires='>=3.10',
     # entry_points={
     #     'console_scripts': [
