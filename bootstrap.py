e9b0b8cf2b3d096dd0b7d6fd48fc95fe721d68d9
jererc <jererc@gmail.com>
2024-11-18 16:37:31 +0400

    update

--- bootstrap.py
+++ bootstrap.py
@@ -1,13 +1,13 @@
 import os
 import urllib.request
 
-cwd = os.path.join(os.path.dirname(__file__))
+cwd = os.path.dirname(os.path.realpath(__file__))
 url = 'https://raw.githubusercontent.com/jererc/svcutils/refs/heads/main/svcutils/bootstrap.py'
 exec(urllib.request.urlopen(url).read().decode('utf-8'))
-
 Bootstrap(
     script_name='test_svc',
-    script_args=[],
+    script_module='test_svc.main',
+    script_args=['collect', '--path', cwd, '--task'],
     install_requires=[
         'test_svc @ https://github.com/jererc/test_svc/archive/refs/heads/main.zip',
     ],
